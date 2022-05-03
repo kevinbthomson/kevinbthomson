aa007c1de633a3641370cb7653b6264517d323c7
Kevin Thomson <kevinthomson@Kevins-MacBook-Air.local>
2022-05-03 14:28:21 -0600

    postcss architecture

--- .eleventy.js
+++ .eleventy.js
@@ -9,10 +9,10 @@ module.exports = function (eleventyConfig) {
 
   return {
     dir: {
-      input: 'src',
-      includes: '_includes',
       data: '_data',
-      output: '_site',
+      includes: '_includes',
+      input: 'src',
+      output: 'public',
     },
   };
 };
